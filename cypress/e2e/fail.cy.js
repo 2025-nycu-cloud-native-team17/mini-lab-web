ca2ef7952a51a6ae77126293347eb8dfcd1a26b5
jteng2127 <jteng2127@MacBook-Air.local>
2025-06-04 07:43:56 +0800

    test: remove cypress failed test and fix test

--- cypress/e2e/fail.cy.js
+++
@@ -1,7 +0,0 @@
-// cypress/e2e/must-fail.cy.js
-describe('Must fail test', () => {
-    it('should always fail', () => {
-        cy.visit('/');
-        cy.get('#this-element-does-not-exist', { timeout: 2000 });
-    })
-})
